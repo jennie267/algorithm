0564fc342d0f33ab8eac22ac2bbaffbfad45c4f8
sooki <jennieee267@gmail.com>
2021-10-20 10:49:01 +0900

    프로그래머스 : 네트워크

---
+++ src/programmers/dfsbfs/network/Network.java
@@ -0,0 +1,52 @@
+package programmers.dfsbfs.network;
+
+/**
+ * 네트워크 level 3
+ * 
+ * https://programmers.co.kr/learn/courses/30/lessons/43162
+ * 
+ * @author js.lee
+ *
+ */
+public class Network {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+
+		int n1 = 3;
+		int[][] computers1 = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+		System.out.println(solution.solution(n1, computers1)); // 2
+
+		int n2 = 3;
+		int[][] computers2 = { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } };
+		System.out.println(solution.solution(n2, computers2)); // 1
+
+	}
+
+}
+
+class Solution {
+	public int solution(int n, int[][] computers) {
+		int answer = 0;
+		boolean[] visited = new boolean[n];
+		
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				dfs(computers, i, visited);
+				answer++;
+			}
+		}
+		return answer;
+	}
+	
+	boolean[] dfs(int[][] computers, int i, boolean[] visited) {
+		visited[i] = true;
+		
+		for (int j = 0; j < computers.length; j++) {
+			if (i != j && computers[i][j] == 1 && visited[j] == false) {
+				visited = dfs(computers, j, visited);
+			}
+		}
+		return visited;
+	}
+}
