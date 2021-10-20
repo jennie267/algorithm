f5873e1ce4db22a8d12a97b910ed52bd2fe16853
sooki <jennieee267@gmail.com>
2021-10-20 16:08:38 +0900

    프로그래머스 : 단어 변환

--- src/programmers/dfsbfs/word/WordBfs.java
+++ src/programmers/dfsbfs/word/WordBfs.java
@@ -1,46 +1,74 @@
 package programmers.dfsbfs.word;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * 단어 변환 level 3
+ * 단어 변환 level 3 (bfs)
  * 
  * https://programmers.co.kr/learn/courses/30/lessons/43163
  * 
  * @author js.lee
  *
  */
-public class Word {
+public class WordBfs {
 
 	public static void main(String[] args) {
-		Solution solution = new Solution();
+		SolutionBfs solution = new SolutionBfs();
 		String begin = "hit";
 		String target = "cog";
 		String[] words1 = { "hot", "dot", "dog", "lot", "log", "cog" };
 		String[] words2 = { "hot", "dot", "dog", "lot", "log" };
 		System.out.println(solution.solution(begin, target, words1)); // 4
-		Solution solution2 = new Solution();
+		SolutionBfs solution2 = new SolutionBfs();
 		System.out.println(solution2.solution(begin, target, words2)); // 0
 
 	}
 
 }
 
-class Solution {
-	int answer;
-	boolean[] visited;
+class SolutionBfs {
+
+	static class Node {
+		String current;
+		int edge;
+
+		public Node(String next, int edge) {
+			this.current = next;
+			this.edge = edge;
+		}
+	}
 
 	public int solution(String begin, String target, String[] words) {
-		answer = words.length;
-		visited = new boolean[words.length];
-
+		int answer = 0;
+		
 		if (!checkInclude(target, words)) {
-			return 0;
+			return answer;
 		}
 
-		dfs(begin, target, words, 0);
+		Queue<Node> queue = new LinkedList<>();
+
+		boolean[] visited = new boolean[words.length];
+		queue.add(new Node(begin, 0));
+
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();
+			if (node.current.equals(target)) {
+				answer = node.edge;
+				break;
+			}
+
+			for (int i = 0; i < words.length; i++) {
+				if (!visited[i] && checkChangeable(node.current, words[i])) {
+					visited[i] = true;
+					queue.add(new Node(words[i], node.edge + 1));
+				}
+			}
+		}
 
 		return answer;
 	}
-
+	
 	boolean checkInclude(String target, String[] words) {
 		for (String word : words) {
 			if (target.equals(word)) {
@@ -59,22 +87,6 @@ class Solution {
 			}
 		}
 		return count == begin.length() - 1 ? true : false;
-	}
-
-	void dfs(String begin, String target, String[] words, int count) {
-		if (begin.equals(target)) {
-			if (answer > count) {
-				answer = count;
-			}
-		}
-		
-		for (int i = 0; i < words.length; i++) {
-			if (!visited[i] && checkChangeable(begin, words[i])) {
-				visited[i] = true;
-				dfs(words[i], target, words, count + 1);
-				visited[i] = false;
-			}
-		}
 	}
 
 }
