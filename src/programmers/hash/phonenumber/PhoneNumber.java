67167877624bdfbb6ccf68231331b6c8c3abefa3
sooki <jennieee267@gmail.com>
2021-10-21 22:34:02 +0900

    refactor : 전화번호 목록 속도 좀 빠르게 개선

--- src/programmers/hash/phonenumber/PhoneNumber.java
+++ src/programmers/hash/phonenumber/PhoneNumber.java
@@ -1,5 +1,7 @@
 package programmers.hash.phonenumber;
 
+import java.util.Arrays;
+
 /**
  * 전화번호 목록
  * level2
@@ -19,14 +21,9 @@ public class PhoneNumber {
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
         for (int i = 0; i < phone_book.length - 1; i++) {
-            for (int j = 0; j < phone_book.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (phone_book[i].startsWith(phone_book[j])) {
-                    return false;
-                }
+            for (int j = i+1; j < phone_book.length; j++) {
                 if (phone_book[j].startsWith(phone_book[i])) {
                     return false;
                 }
