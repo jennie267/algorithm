273a29ad45b95b32ba70165249ff5516ccea7414
sooki <jennieee267@gmail.com>
2021-10-21 23:07:08 +0900

    refactor : 전화번호 효율성 3,4번 통과

--- src/programmers/hash/phonenumber/PhoneNumber.java
+++ src/programmers/hash/phonenumber/PhoneNumber.java
@@ -22,11 +22,9 @@ public class PhoneNumber {
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            for (int j = i+1; j < phone_book.length; j++) {
-                if (phone_book[j].startsWith(phone_book[i])) {
-                    return false;
-                }
+        for (int i = 0; i < phone_book.length - 1; i++){
+            if (phone_book[i + 1].startsWith(phone_book[i])){
+                return false;
             }
         }
         return true;
