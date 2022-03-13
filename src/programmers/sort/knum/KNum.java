6ed12c5a680f1dce926536404e86ffef06b77802
jennie267 <jennieee267@gmail.com>
2022-03-13 23:58:22 +0900

    프로그래머스 : K번째수 (정렬)

---
+++ src/programmers/sort/knum/KNum.java
@@ -0,0 +1,38 @@
+package programmers.sort.knum;
+
+import java.util.stream.IntStream;
+
+/**
+ * K번째수
+ * level 1
+ * 정렬
+ * https://programmers.co.kr/learn/courses/30/lessons/42748
+ */
+public class KNum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = new int[]{1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        int[] result = solution.solution(array, commands);
+    }
+    
+}
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        int cursor = 0;
+        for (int[] command : commands) {
+            int i = command[0]-1;
+            int j = command[1];
+            int k = command[2]-1;
+
+            int[] newArray = IntStream.range(i, j).map(a -> array[a]).toArray();
+            answer[cursor++] = newArray[k];
+        }
+
+        return answer;
+    }
+}
