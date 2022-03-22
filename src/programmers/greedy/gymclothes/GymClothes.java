343092513b380baed4cca1a13564a57e7d5bd03a
jennie267 <jennieee267@gmail.com>
2022-03-22 23:47:34 +0900

    프로그래머스 - 체육복 (greedy)

---
+++ src/programmers/greedy/gymclothes/GymClothes.java
@@ -0,0 +1,13 @@
+package programmers.greedy.gymclothes;
+
+/**
+ * 체육복
+ * level 1
+ * 탐욕법 (Greedy)
+ * https://programmers.co.kr/learn/courses/30/lessons/42862
+ */
+public class GymClothes {
+    public static void main(String[] args) {
+
+    }
+}
